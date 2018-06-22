2a18435fd31be815242c7280c4166e9e7c1ea67d
gjpqddggy <gjpqddggy@gmail.com>
2018-06-22 18:10:29 +0800

    update: menu.cpp menu.h

--- final_project/Menu.cpp
+++ final_project/Menu.cpp
@@ -1,7 +1,7 @@
 #include "Menu.h"
 
-const int ThumbWidth = 50;
-const int ThumbHeight = 50;
+const int ThumbWidth = 100;
+const int ThumbHeight = 100;
 const int gapX = 40, gapY = 30;
 const int offsetX = 30 + field_width, offsetY = 150;
 const int Initial_Health = 10;
@@ -16,16 +16,6 @@ Menu::isInRange(int point, int startPos, int length)
     return false;
 }
 
-bool
-Menu::Enough_Coin(int type)
-{
-    
-    if(type < 0 || type >= Num_TowerType)
-        return false;
-    
-    return (Coin + need_coin[type] >= 0);
-}
-
 Menu::Menu()
 {
     char filename[50];
@@ -34,16 +24,16 @@ Menu::Menu()
     Coin = Initial_Coin;
     Score = 0;
     
-    for(int i=0;i<Num_TowerType; i++)
+    for(int i=1;i<=Num_TankType; i++)
     {
-        ALLEGRO_BITMAP *tower;
-        sprintf(filename, "./Tower/%s_Menu.png", TowerClass[i]);
+        ALLEGRO_BITMAP *tank;
+        sprintf(filename, "./RedTank/%d_Menu.png", i);
         
-        tower = al_load_bitmap(filename);
-        menu_tower.push_back(tower);
+        tank = al_load_bitmap(filename);
+        menu_tank.push_back(tank);
     }
     
-    love = al_load_bitmap("./Tower/love.png");
+    love = al_load_bitmap("./RedTank/love.png");
     
     menuFont = al_load_ttf_font("pirulen.ttf", 12, 0); // load font
 }
@@ -53,10 +43,10 @@ Menu::~Menu()
     al_destroy_bitmap(love);
     al_destroy_font(menuFont);
     
-    for(int i=0; i < Num_TowerType; i++)
-        al_destroy_bitmap(menu_tower[i]);
+    for(int i=0; i < Num_TankType; i++)
+        al_destroy_bitmap(menu_tank[i]);
     
-    menu_tower.clear();
+    menu_tank.clear();
 }
 
 void
@@ -65,7 +55,7 @@ Menu::Reset()
     HealthPoint = Initial_Health;
     Coin = Initial_Coin;
     Score = 0;
-    killedMonster = 0;
+    killedTank = 0;
 }
 
 void
@@ -77,50 +67,29 @@ Menu::Draw()
     al_draw_bitmap(love, offsetX, 20, 0);
     al_draw_text(menuFont, al_map_rgb(255, 255, 255), 2*al_get_bitmap_width(love) + offsetX, 20, 0, buffer);
     
-    sprintf(buffer, "Coin: %d", Coin);
-    al_draw_text(menuFont, al_map_rgb(255, 255, 255), offsetX, 20 + gapY, 0, buffer);
+//    sprintf(buffer, "Coin: %d", Coin);
+//    al_draw_text(menuFont, al_map_rgb(255, 255, 255), offsetX, 20 + gapY, 0, buffer);
     
     sprintf(buffer, "Score: %d", Score);
     al_draw_text(menuFont, al_map_rgb(255, 255, 255), offsetX, 20 + 2*gapY, 0, buffer);
     
-    for(int i=0; i < Num_TowerType; i++)
+    for(int i=0; i < Num_TankType; i++)
     {
         int pos_x = offsetX + (ThumbWidth + gapX) * (i % 2);
         int pos_y = offsetY + (ThumbHeight + gapY) * (i / 2);
         
-        al_draw_bitmap(menu_tower[i], pos_x, pos_y, 0);
-        if(!Enough_Coin(i))
-            al_draw_filled_rectangle(pos_x, pos_y, pos_x + ThumbWidth, pos_y + ThumbHeight, al_map_rgba(100, 100, 100, 100));
-        else if(i == selectedTower)
-            al_draw_rectangle(pos_x, pos_y, pos_x + ThumbWidth, pos_y + ThumbHeight, al_map_rgb(255, 0, 0), 0);
-        else
-            al_draw_rectangle(pos_x, pos_y, pos_x + ThumbWidth, pos_y + ThumbHeight, al_map_rgb(255, 255, 255), 0);
+        al_draw_bitmap(menu_tank[i], pos_x, pos_y, 0);
+        al_draw_rectangle(pos_x, pos_y, pos_x + ThumbWidth, pos_y + ThumbHeight, al_map_rgb(255, 255, 255), 0);
     }
 }
 
-int
-Menu::MouseIn(int mouse_x, int mouse_y)
-{
-    bool enoughCoin;
-    
-    selectedTower = -1;
-    
-    for(int i=0; i < Num_TowerType; i++)
-    {
-        int pos_x = offsetX + (ThumbWidth + gapX) * (i % 2);
-        int pos_y = offsetY + (ThumbHeight + gapY) * (i / 2);
-        
-        if(isInRange(mouse_x, pos_x, ThumbWidth) && isInRange(mouse_y, pos_y, ThumbHeight))
-        {
-            if(Enough_Coin(i))
-            {
-                selectedTower = i;
-                break;
-            }
-        }
-    }
-    return selectedTower;
-}
+//int
+//Menu::MouseIn(int mouse_x, int mouse_y)
+//{
+//    
+//    
+//    
+//}
 
 bool
 Menu::Subtract_HP(int escapeNum)
@@ -133,7 +102,7 @@ Menu::Subtract_HP(int escapeNum)
 void
 Menu::Gain_Score(int scoreWorth)
 {
-    killedMonster++;
+    killedTank++;
     Score += scoreWorth;
 }
 
