efd0f8074003f97c7c18d2d7ef552453ea40636c
gjpqddggy <gjpqddggy@gmail.com>
2018-06-22 17:34:20 +0800

    update: global.cpp global.h

--- final_project/global.cpp
+++ final_project/global.cpp
@@ -1,12 +1,1 @@
 #include "global.h"
-
-/*int window_width = 800;
- int window_height = 600;
- int field_width = 600;
- int field_height = 600;
- int NumOfTower = 5;*/
-
-int TowerRadius[] = {80, 80, 70, 110, 150, 150};
-char TowerClass[][20] = {"Arcane", "Archer", "Canon", "Poison", "Storm", "Magma"};
-int TowerWidth[] = {72, 66, 72, 59, 45, 53};
-int TowerHeight[] = {121, 108, 69, 98, 112, 94};
