2b6b1af2637f365ad323edaccb9d325162eb07ab
gjpqddggy <gjpqddggy@gmail.com>
2018-06-24 05:42:55 +0800

    Update: global 	define four new number: game_start( x,  y ) / game_end( x, y )

--- final_project/global.h
+++ final_project/global.h
@@ -11,6 +11,10 @@
 #define window_height 1440
 #define field_width 1500
 #define field_height 1300
+#define game_start_x 70
+#define game_start_y 70
+#define game_end_x 1620
+#define game_end_y 1370
 
 #define Num_TankType 4
 #define NumOfGrid (field_width / grid_width) * (field_height / grid_height) // 15 * 13 = 195
