fe804f7ccb9e768c4f360ab6c8483b780a880f9f
geep604 <geep80108@gmail.com>
2018-06-25 01:29:03 +0800

    beams

---
+++ final_project/Obstacle.h
@@ -0,0 +1,13 @@
+//
+//  Obstacle.h
+//  final_project
+//
+//  Created by 景璞 on 2018/6/25.
+//  Copyright © 2018年 Sonic Wang. All rights reserved.
+//
+
+#ifndef Obstacle_h
+#define Obstacle_h
+
+
+#endif /* Obstacle_h */
